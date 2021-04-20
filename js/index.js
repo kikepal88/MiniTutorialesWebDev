f567d6d276e217f49b79c7000d8dd7bb963c8918
Omar Palacios <omarpal88@gmail.com>
2021-04-20 18:19:40 -0500

    Se agrega un nuevo icono al repo, se hacen cambios en el HTML para permitir cargar JS y poder llamar elementos desde el mismo, se ajustan estilos para el funcionamiento del header en version de 375px

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function showMobileMenu () {
+  document.getElementById('header__icon-menu').classList.toggle('icon-close');
+  document.getElementById('header__nav').classList.toggle('is-active');
+  document.getElementById('header__section').classList.toggle('menu__is-active');
+}
