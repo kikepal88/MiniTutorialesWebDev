18eea5c6a89d462827889070921983353aec4347
Omar Palacios <omarpal88@gmail.com>
2021-04-29 20:03:41 -0500

    Se agregan IDs a la estructura HTML para manipular el slider, se agregan algunos estilos al slider y se configura la logica del slider en JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,38 @@
+const TutorialSlider = document.querySelector("#tutorial__slider");
+let imgTutorials = document.querySelectorAll(".img__container");
+let imgTutorialsLast = imgTutorials[imgTutorials.length - 1];
+
+const buttonBack = document.querySelector("button__back");
+const buttonNext = document.querySelector("button__next");
+
+TutorialSlider.insertAdjacentElement('afterbegin', imgTutorialsLast);
+
+function Next() {
+  let imgTutorialsFirst = document.querySelectorAll(".img__container")[0];
+  TutorialSlider.style.marginInlineStart = "-200%";
+  TutorialSlider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    TutorialSlider.style.transition = "none";
+    TutorialSlider.insertAdjacentElement('beforeend', imgTutorialsFirst);
+    TutorialSlider.style.marginInlineStart = "-100%";
+  }, 500);
+}
+
+function Back() {
+  let imgTutorials = document.querySelectorAll(".img__container");
+  let imgTutorialsLast = imgTutorials[imgTutorials.length - 1];
+  TutorialSlider.style.marginInlineStart = "0";
+  TutorialSlider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    TutorialSlider.style.transition = "none";
+    TutorialSlider.insertAdjacentElement('afterbegin', imgTutorialsLast);
+    TutorialSlider.style.marginInlineStart = "-100%";
+  }, 500);
+}
+
+setInterval(function() {
+  Next();}, 7000);
+
 function showMobileMenu () {
   document.getElementById('header__icon-menu').classList.toggle('icon-close');
   document.getElementById('header__nav').classList.toggle('is-active');
